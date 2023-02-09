Fixed a bug with numerical columns not reflecting weights of Missing Data and removed unused imports

exhibit/core/constraints.py
@@ -16,9 +16,8 @@ from scipy.stats import beta, gamma
 from pandas.api.types import is_numeric_dtype
 
 # Exibit imports
-from .sql import query_exhibit_database
 from .generate.continuous import scale_continuous_column
-from .constants import ORIGINAL_VALUES_DB, ORIGINAL_VALUES_PAIRED, MISSING_DATA_STR
+from .constants import MISSING_DATA_STR
 from .generate.geo import geo_make_regions
 from .linkage.matrix import generate_user_linked_anon_df
 from .utils import natural_key

exhibit/core/exhibit.py
@@ -384,7 +384,8 @@ class Exhibit:
         # rearrange (sort, make distinct or same) the categorical values leaving
         # the original numerical values that don't correspond to correct weights
         constraint_targets = []
-        cat_cols_set = set(self.spec_dict["metadata"]["categorical_columns"])
+        cat_cols = self.spec_dict["metadata"]["categorical_columns"]
+        cat_cols_set = set(cat_cols)
 
         if (ccs:=self.spec_dict["constraints"]["custom_constraints"]):
             
@@ -395,6 +396,9 @@ class Exhibit:
 
         # check if there are common columns between constraint targets and cat_cols
         if cat_cols_set & set(constraint_targets):
+
+            anon_df.loc[:, cat_cols] = (
+                anon_df.loc[:, cat_cols].fillna(MISSING_DATA_STR))
             
             # we don't want to re-create continuous columns that were subject of a cc
             num_cols = (
@@ -416,6 +420,9 @@ class Exhibit:
                     anon_df=anon_df.loc[~num_col_na_mask],
                     col_name=num_col
                 )
+            
+            anon_df.loc[:, cat_cols] = anon_df.loc[:, cat_cols].applymap(
+            lambda x: np.nan if x == MISSING_DATA_STR else x)
 
         #8) GENERATE DERIVED COLUMNS IF ANY ARE SPECIFIED
         for name, calc in self.spec_dict["derived_columns"].items():