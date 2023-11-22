Fixed a bug with scaling continuous columns that could result in null values

exhibit/core/constraints.py
@@ -1417,7 +1417,7 @@ def _get_probabilities_by_frequency(f, target_f="high"):
     
     Parameters
     ----------
-    f             : np.array or Sequence
+    f             : np.ndarray or Sequence
         a sequence of row frequencies sorted large to small
     target_f      : str
         preferred direction, either high or low.
@@ -1439,7 +1439,7 @@ def _get_probabilities_by_frequency(f, target_f="high"):
     '''
 
     if not isinstance(f, np.ndarray): #pragma: no cover
-        f = np.array
+        f = np.ndarray(f)
 
     if target_f == "high":
 

exhibit/core/generate/continuous.py
@@ -234,7 +234,9 @@ def _scale_to_range(series, precision, target_min=None, target_max=None, **_kwar
     
     # where the min == max in the source data, just pick non-null target and fill series
     if len(X.unique()) == 1:
-        return pd.Series(np.full(shape=len(X), fill_value=target_min or target_max))
+        return pd.Series(
+            np.full(shape=len(X), fill_value=target_min or target_max),
+            name=series.name)
     
     # adjust for potential negative signs!
     if target_min is None:
@@ -254,7 +256,8 @@ def _scale_to_range(series, precision, target_min=None, target_max=None, **_kwar
         labels = np.arange(np.floor(target_min), np.ceil(target_max) + 1)
 
         out = pd.Series(
-            pd.cut(X, bins=bins, right=True, include_lowest=True, labels=labels)
+            pd.cut(X, bins=bins, right=True, include_lowest=True, labels=labels),
+            name=series.name
         )
 
         return out.astype("Int64")
@@ -343,7 +346,8 @@ def _conditional_rounding(series, target_sum):
             series + row_diff >= 0,
             series + row_diff,
             np.where(pd.isnull(series), pd.NA, 0)
-            )
+            ),
+        name=series.name
     )
     
     #how many rows will need to be rounded up to get to target
@@ -353,7 +357,7 @@ def _conditional_rounding(series, target_sum):
     #to adjust them to a lower target_sum; we floor them and return
     if boundary < 0:
         print(f"Target sum for {series.name} is too low for the number of rows.")
-        return pd.Series(np.floor(values))
+        return pd.Series(np.floor(values), name=series.name)
     
     #if series has NAs, then the calcualtion will be off
     clean_values = values.dropna() #keep original index

exhibit/core/generate/tests/test_continuous.py
@@ -463,7 +463,8 @@ class continuousTests(unittest.TestCase):
         '''
 
         test_series = pd.Series(
-            [1, 2, 4, 8, 16, 4, 1]
+            [1, 2, 4, 8, 16, 4, 1],
+            name="test"
         )
             
         test_min = 10
@@ -487,6 +488,7 @@ class continuousTests(unittest.TestCase):
 
         self.assertEqual(scaled_series.min(), test_min)
         self.assertEqual(scaled_series.max(), test_max)
+        self.assertEqual(scaled_series.name, test_series.name)
 
     def test_range_scaling_with_integer_precision(self):
         '''
@@ -500,7 +502,8 @@ class continuousTests(unittest.TestCase):
         '''
 
         test_series = pd.Series(
-            [1, 2, 4, 8, 16, 4, 1, np.nan]
+            [1, 2, 4, 8, 16, 4, 1, np.nan],
+            name="test"
         )
             
         test_min = 10
@@ -513,6 +516,7 @@ class continuousTests(unittest.TestCase):
         self.assertEqual(scaled_series.min(), test_min)
         self.assertEqual(scaled_series.max(), test_max)
         self.assertEqual(scaled_series.dtype, "Int64")
+        self.assertEqual(scaled_series.name, test_series.name)
 
     def test_range_scaling_with_missing_min_max(self):
         '''
@@ -542,7 +546,10 @@ class continuousTests(unittest.TestCase):
         notebook in the docs folder of Exhibit.
         '''
 
-        test_series = pd.Series(np.random.normal(loc=10, scale=1, size=1_000))
+        test_series = pd.Series(
+            np.random.normal(loc=10, scale=1, size=1_000),
+            name="test"
+        )
             
         test_mean = -20
         test_std = 5
@@ -553,6 +560,28 @@ class continuousTests(unittest.TestCase):
 
         self.assertAlmostEqual(scaled_series.mean(), test_mean)
         self.assertAlmostEqual(scaled_series.std(), test_std)
+        self.assertEqual(scaled_series.name, test_series.name)
+
+    def test_scale_to_statistic_sum_series_name(self):
+        '''
+        We need to ensure that that the scaling function returns a correctly scaled
+        series with a name that matches the source, otherwise bugs can appear when,
+        for example, concatenating the results.
+        '''
+
+        test_series = pd.Series(
+            np.random.normal(loc=10, scale=1, size=1_000),
+            name="test"
+        )
+            
+        test_sum=500
+        precision = "integer"
+
+        scaled_series = tm._scale_to_target_sum(
+            test_series, precision, target_sum=test_sum)
+
+        self.assertEqual(scaled_series.sum(), test_sum)
+        self.assertEqual(scaled_series.name, test_series.name)
 
     def test_range_scaling_with_equal_weights(self):
         '''